2a2dc6bd2247dcd63d38ce56ba4ff12ac533a806
Shahmir Khan Jadoon <sjaoon@ssidecisions.com>
2022-02-08 16:01:23 +0500

    Configure and use useTypedSelector hook

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,19 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: FC = () => {
+	const [term, setTerm] = useState('');
+	const { searchRepositories } = useActions();
+
+	const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		searchRepositories(term);
+	};
+
 	return (
 		<div>
-			<form>
-				<input type='text' />
+			<form onSubmit={onSubmitHandler}>
+				<input type='text' value={term} onChange={e => setTerm(e.target.value)} />
 				<button>Search</button>
 			</form>
 		</div>
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk)); // {} is the initial state
+export const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(thunk))); // {} is the initial state
